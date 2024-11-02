80efd016997b5b2301b6886e515a6e2015259e4e
jmbarron1 <184740221+jmbarron1@users.noreply.github.com>
2024-11-02 07:21:55 -0400

    Add 'no-sandbox' flag to appArgs in wdio.conf_build_agent.js

--- wdio.conf_build_agent.js
+++ wdio.conf_build_agent.js
@@ -55,7 +55,7 @@ exports.config = {
         // see https://webdriver.io/docs/desktop-testing/electron/configuration/#service-options
         'wdio:electronServiceOptions': {
             // custom application args
-            appArgs: ['headless', 'disable-gpu']
+            appArgs: ['headless', 'disable-gpu', 'no-sandbox']
         }
     }],
 
